c21e41e63c27ebde69732ab5941708dd2afb6e06
Nitish21073 <nitish21073@gmail.com>
2023-02-06 12:38:24 +0530

    env var setting

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cfg8ocha6gdma8mu6c2g-a.singapore-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'sample_6lac'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', 'qXOTbYu48WSEWr5SNg7vgtgyOl1yS8rE'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
